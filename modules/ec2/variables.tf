aff05e36c8f1a2b1dfc9246bde5f57097af46800
shubi042 <shubi0411@gmail.com>
2026-06-03 10:44:03 -0400

    Add EC2 variables

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,9 @@
+variable "project_name"      { type = string }
+variable "environment"        { type = string }
+variable "vpc_id"             { type = string }
+variable "public_subnet_ids"  { type = list(string) }
+variable "private_subnet_ids" { type = list(string) }
+variable "instance_type"      { type = string }
+variable "min_size"           { type = number }
+variable "max_size"           { type = number }
+variable "desired_capacity"   { type = number }
